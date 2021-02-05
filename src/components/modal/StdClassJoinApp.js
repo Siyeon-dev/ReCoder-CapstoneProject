c205bd13d42e70eb5ee87fef83731cedbab79182
LeeGuSeul <rntmf1247@naver.com>
2021-02-05 18:53:56 +0900

    Select Box State

--- src/components/modal/StdClassJoinApp.js
+++ src/components/modal/StdClassJoinApp.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useModal from "./useModal";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
 const StdClassJoinApp = ({ appllyStdList, classCode }) => {
-  const [isOpen, setIsOpen, Modal] = useModal();  
-  const [selectStdAppList, setSelectStdAppList] = useState([]);
-  const bbb = [];
-  const uniqueArr2 = [];
+  const stdList = appllyStdList;
+  const [isOpen, setIsOpen, Modal] = useModal(false);  
+  const [array, setArray] = useState([]);
+  const [test, setTest] = useState(false);
+  useEffect(() => {
+    console.log(array);
+  }, [array]);
+  useEffect(() => { console.log(appllyStdList) })
+  
+  const aaa = (e) => {
+    console.log(e.target.checked)
+    console.log(typeof array)
+    setArray([...array, e.target.checked])
+  }
 
-  console.log(appllyStdList);
+  // const appllyStdListSetUp = () => {
+  //   return (
+      
+  //   );
+  // };
   
   const valueChecked = () => { 
       if (appllyStdList) {
@@ -23,24 +37,9 @@ const StdClassJoinApp = ({ appllyStdList, classCode }) => {
       }
   }
   
-      const handleCheck = (e) => {
-        bbb.push(e.target.name);
-
-        bbb.forEach((element) => {
-          if (!uniqueArr2.includes(element)) {
-            uniqueArr2.push(element);
-          } else {
-            console.log(element);
-            uniqueArr2.splice(uniqueArr2.indexOf(element), 1);
-          }
-        });
-        bbb.length = 0;
-        console.log(uniqueArr2);
-      };
 
 
   const appllyStdListSubmit = () => {
-    console.log("asdasd");
       axios
         .post("/classrecognize", valueChecked())
         .then((res) => {
@@ -54,23 +53,22 @@ const StdClassJoinApp = ({ appllyStdList, classCode }) => {
         });
     }
 
-  const appllyStdListSetUp = appllyStdList.map((currElement, index) =>
-    console.log(appllyStdList);
-      currElement.recognize === 0 ? (
-        <div className="class_list_check">
-          <input
-            type="checkbox"
-            id={index}
-            name={currElement.s_email}
-            onChange={() => {handleCheck}}
-          />
-          <label for={index}>
-            {currElement.s_name}
-            <span className="std_email">{currElement.s_email}</span>
-          </label>
-        </div>
-      ) : null
-    );
+    // const appllyStdListSetUp = appllyStdList.map((currElement, index) =>
+    //   currElement.recognize === 0 ? (
+    //     <div className="class_list_check">
+    //       <input
+    //         type="checkbox"
+    //         id={index}
+    //         name={currElement.s_email}
+    //         onChange={}
+    //       />
+    //       <label for={index}>
+    //         {currElement.s_name}
+    //         <span className="std_email">{currElement.s_email}</span>
+    //       </label>
+    //     </div>
+    //   ) : null
+    // );
 
   // classinfo API 완료 후 클래스 회원 가입 승인 작업
   return (
@@ -85,16 +83,35 @@ const StdClassJoinApp = ({ appllyStdList, classCode }) => {
                 클래스 가입을 신청한 학생들을 확인 및 승인을 할 수 있습니다.
               </p>
             </div>
-            <form onSubmit={(e) => {
-              e.preventDefault();
-              appllyStdListSubmit()
-            }}>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                appllyStdListSubmit();
+              }}
+            >
               <div className="class_list_area">
-                {appllyStdList.length === 0 ? (
+                {/* {appllyStdList.length === 0 ? (
                   <p className="no_class_list">가입 신청한 학생이 없습니다.</p>
-                ) : (
-                  appllyStdListSetUp
-                )}
+                ) : 
+                  () => appllyStdListSetUp()
+                } */}
+                {/* {appllyStdListSetUp()} */}
+                <>
+        {appllyStdList &&
+          stdList.map((v) => (
+            <div className="class_list_check">
+                <input
+                  type={"checkbox"}
+                id={v.index}
+                onChange={aaa}
+                />
+              <label for={v.index}>
+                {v.s_name}
+                <span className="std_email">{v.s_email}</span>
+              </label>
+            </div>
+          ))}
+      </>
               </div>
               <button type="submit">가입승인</button>
             </form>
