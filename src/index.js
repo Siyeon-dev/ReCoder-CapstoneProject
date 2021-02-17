b7521d28fbae171431e4bd82a8367a66fdca9980
kanbaer-sht <kanbaer.sht@gmail.com>
2021-02-17 18:27:34 +0900

    add LogIn and Validation for abnormal access::2021-02-17

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from "./components/Main";
-import Guide from "./components/Guide";
 import Router from './components/Router';
-import { Route, HashRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
